26d178d7888d24bf5bbe2013e18a4e3e5f581dad
elvinaliyev6 <elvinaliyevinfo@gmail.com>
2022-07-23 23:09:16 +0400

    employee management rest api

---
+++ src/main/java/az/spring/employeemanagementrestapi/EmployeeManagementRestApiApplication.java
@@ -0,0 +1,13 @@
+package az.spring.employeemanagementrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/spring/employeemanagementrestapi/EmployeeManagementRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package az.spring.employeemanagementrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
